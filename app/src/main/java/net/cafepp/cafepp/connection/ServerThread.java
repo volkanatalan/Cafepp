46e5db1b3f0eab7eda5fd013a32184828480ee3e
Volkan ATALAN <volkanatalan35@gmail.com>
2019-04-16 01:05:41 +0300

    Minor changes

--- app/src/main/java/net/cafepp/cafepp/connection/ServerThread.java
+++ app/src/main/java/net/cafepp/cafepp/connection/ServerThread.java
@@ -13,7 +13,6 @@ public class ServerThread implements Runnable {
   private Context context;
   private ServerSocket serverSocket;
   private Device myDevice;
-  Thread threadCommunication;
   
   public ServerThread(Context context, ServerSocket serverSocket) {
     this.context = context;
@@ -25,28 +24,22 @@ public class ServerThread implements Runnable {
   
     while (!Thread.currentThread().isInterrupted()) {
       if (serverSocket != null && !serverSocket.isClosed()) {
-      try {
-        Log.d(TAG, "Listening for the inputs");
-        CommunicationThread communicationThread = new CommunicationThread(
-            context, serverSocket.accept(), CommunicationThread.Command.LISTEN);
-        communicationThread.setMyDevice(myDevice);
-        threadCommunication = new Thread(communicationThread);
-        threadCommunication.start();
-      
-      } catch (IOException e) {
-        e.printStackTrace();
-        Log.e(TAG, "Error Communicating to Client:" + e.getMessage());
-      }
-    } else Log.e(TAG, "Server socket is null!");
+        try {
+          Log.d(TAG, "Listening for the inputs...");
+          CommunicationThread communicationThread = new CommunicationThread(
+              context, serverSocket.accept(), CommunicationThread.Command.LISTEN);
+          communicationThread.setMyDevice(myDevice);
+          new Thread(communicationThread).start();
+        
+        } catch (IOException e) {
+          Log.e(TAG, "Error Communicating to Client!");
+          e.printStackTrace();
+        }
+        
+      } else Log.e(TAG, "Server socket is null!");
     }
   
-    Log.d(TAG, "Thread is interrupted");
-    
-    try {
-      if (!serverSocket.isClosed()) serverSocket.close();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+    Log.d(TAG, "ServerThread is interrupted");
     
   }
   
