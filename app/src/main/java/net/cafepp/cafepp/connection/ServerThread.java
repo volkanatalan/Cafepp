75e52369f10b8bfb1449c50b2f3587bf1a61d2de
Volkan ATALAN <volkanatalan35@gmail.com>
2019-04-20 16:02:47 +0300

    Input listener added.

--- app/src/main/java/net/cafepp/cafepp/connection/ServerThread.java
+++ app/src/main/java/net/cafepp/cafepp/connection/ServerThread.java
@@ -1,6 +1,5 @@
 package net.cafepp.cafepp.connection;
 
-import android.content.Context;
 import android.util.Log;
 
 import net.cafepp.cafepp.objects.Device;
@@ -10,44 +9,58 @@ import java.net.ServerSocket;
 
 public class ServerThread implements Runnable {
   private final String TAG = "ServerThread";
-  private Context context;
+  private Package mPackage;
   private ServerSocket serverSocket;
-  private Device myDevice;
   
-  public ServerThread(Context context, ServerSocket serverSocket) {
-    this.context = context;
+  public ServerThread(ServerSocket serverSocket) {
     this.serverSocket = serverSocket;
   }
   
   @Override
   public void run() {
   
-    while (!Thread.currentThread().isInterrupted()) {
-      if (serverSocket != null && !serverSocket.isClosed()) {
-        try {
-          Log.d(TAG, "Listening for the inputs...");
-          CommunicationThread communicationThread = new CommunicationThread(
-              context, serverSocket.accept(), CommunicationThread.Command.LISTEN);
-          communicationThread.setMyDevice(myDevice);
-          new Thread(communicationThread).start();
+    if (mPackage != null) {
+      Command command = mPackage.getCommand();
+      Device myDevice = mPackage.getSendingDevice();
+  
+      while (!Thread.currentThread().isInterrupted()) {
+        if (serverSocket != null && !serverSocket.isClosed()) {
+          try {
+            Log.d(TAG, "Listening for the inputs...");
         
-        } catch (IOException e) {
-          Log.e(TAG, "Error Communicating to Client!");
-          e.printStackTrace();
-        }
+            CommunicationThread communicationThread =
+                new CommunicationThread(serverSocket.accept(), command);
+            communicationThread.setMyDevice(myDevice);
+            communicationThread.setOnInputListener(aPackage -> {
+              if (onPackageInputListener != null) onPackageInputListener.onReceive(aPackage);
+            });
         
-      } else Log.e(TAG, "Server socket is null!");
+            new Thread(communicationThread).start();
+        
+          } catch (IOException e) {
+            Log.e(TAG, "Error Communicating to Client!");
+            e.printStackTrace();
+          }
+      
+        } else Log.e(TAG, "Server socket is null!");
+      }
     }
   
     Log.d(TAG, "ServerThread is interrupted");
     
   }
   
-  public Device getMyDevice() {
-    return myDevice;
+  private OnPackageInputListener onPackageInputListener;
+  
+  public interface OnPackageInputListener {
+    void onReceive(Package aPackage);
   }
   
-  public void setMyDevice(Device myDevice) {
-    this.myDevice = myDevice;
+  public void setOnPackageInputListener(OnPackageInputListener listener) {
+    onPackageInputListener = listener;
+  }
+  
+  public void setPackage(Package pkg) {
+    mPackage = pkg;
   }
 }
